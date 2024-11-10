fc7fc4567f2d465daa56360f3f5f918b6afd9f26
Ashley Williams <ashleyw@beigetech.com.au>
2024-11-10 16:09:05 +1000

    added recipe DTO and json deserialiser

--- lib/Recipe.dart
+++ lib/Recipe.dart
@@ -1,6 +1,31 @@
 class Recipe {
   final String title;
+  final String name;
   final String description;
 
-  Recipe(this.title, this.description);
+  const Recipe({
+    required this.title,
+    required this.name,
+    required this.description
+  });
+
+  Recipe.createNew(this.title,
+      this.name,
+      this.description);
+
+  factory Recipe.fromJson(Map<String, Recipe> json) {
+    return switch(json) {
+      {
+      'title': String title,
+      'name': String name,
+      'description': String description
+      } =>
+          Recipe(
+              title: title,
+              name: name,
+              description: description
+          ),
+      _ => throw const FormatException("Failed to process Recipe data"),
+    };
+  }
 }
